e01d98674647a5465999089a6a966e4b0467dd95
Maria Belen Dominguez Sigler <mariabelendominguezsigler@MMBA-B975.local>
2026-05-28 12:36:57 -0300

    created prompts

---
+++ backend/prompts/templates.py
@@ -0,0 +1,18 @@
+SUMMARIZE_PROMPT = """Sos un agente experto  en resumen de textos. Tu tarea es resumir el siguiente texto de manera clara y concisa, manteniendo la esencia y los puntos clave del contenido original. El resumen debe ser fácil de entender y captar la información más importante sin perder el contexto. No podes inventar información adicional, ni inferir cosas que no esten en el texto. Se lo mas fiel al texto original, y no omitas detalles importantes. Recuerda que el objetivo es proporcionar una visión general clara y concisa del contenido, sin perder la esencia de lo que se está resumiendo.
+Texto a resumir:
+{text}"""
+
+IMPROVE_PROMPT = """Sos un agente experto en mejora de textos. Tu tarea es mejorar el texto proporcionado, haciéndolo más claro y fácil de entender, sin perder la esencia y los puntos clave del contenido original.  No podes inventar información adicional, ni inferir cosas que no esten en el texto. Se lo mas fiel al texto original, y no omitas detalles importantes. Recuerda que el objetivo es proporcionar una versión mejorada del contenido, manteniendo la esencia de lo que se está mejorando.
+Texto a mejorar:
+{text}"""
+
+CLASSIFY_PROMPT = """Sos un agente experto en clasificación de textos. Clasificá el texto según su tono (positivo, negativo, neutro) y su temática principal (tecnología, política, deporte, etc.) y el idioma. La clasificación debe ser precisa y relevante, reflejando la esencia del texto original. Cuando lo interpretes para clasificarlo, no inventes  ni infieras cosas que no esten en el texto.  Recuerda que el objetivo es asignar una categoría adecuada al contenido del texto, manteniendo la esencia de lo que se está clasificando.
+Texto a clasificar:
+{text}"""
+
+
+PROMPTS = {
+    "summarize": SUMMARIZE_PROMPT,
+    "improve": IMPROVE_PROMPT,
+    "classify": CLASSIFY_PROMPT,
+}
